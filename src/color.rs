b7330e6da6f0144217786ecda850cd792b8675b6
Nick Hahn <nick.hahn@posteo.de>
2021-12-28 02:27:37 +0100

    Add support for transarent png images

--- src/color.rs
+++ src/color.rs
@@ -6,19 +6,20 @@ pub struct Color {
     r: u8,
     g: u8,
     b: u8,
+    a: u8,
 }
 
 impl Color {
     /// Constructor.
     ///
     /// The color channels must be between 0 and 255.
-    pub fn from(r: u8, g: u8, b: u8) -> Color {
-        Color { r, g, b }
+    pub fn from(r: u8, g: u8, b: u8, a: u8) -> Color {
+        Color { r, g, b, a}
     }
 
     /// Get a hexadecimal representation of the color,
     /// such as `FFFFFF` for white and `FF0000` for red.
     pub fn as_hex(self) -> String {
-        format!("{:02X}{:02X}{:02X}", self.r, self.g, self.b)
+        format!("{:02X}{:02X}{:02X}{:02X}", self.r, self.g, self.b, self.a)
     }
 }
