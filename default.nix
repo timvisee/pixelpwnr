fbb75dd5e295bada3c04aa63749187589bb39cc5
Robert Manigk <p1ng0ut@mailbox.org>
2025-01-02 18:54:02 +0100

    Add simple default.nix

---
+++ default.nix
@@ -0,0 +1,9 @@
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.rustPlatform.buildRustPackage rec {
+  pname = "pixelpwnr";
+  version = "0.1";
+
+  cargoLock.lockFile = Cargo.lock;
+
+  src = pkgs.lib.cleanSource ./.;
+}
